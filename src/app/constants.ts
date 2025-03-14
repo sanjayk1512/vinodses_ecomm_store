69e1c68ca92de0f93e44abc433dd2a5daee2d679
Sanjay K <2001ksanjay@gmail.com>
2025-03-14 21:54:09 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://database-1.c3kaay88gbjs.ap-south-1.rds.amazonaws.com:3306',
+    BACKEND_API_URL: 'http://database-1.c3kaay88gbjs.ap-south-1.rds.amazonaws.com:3306/vinodses_ecomm',
 };
