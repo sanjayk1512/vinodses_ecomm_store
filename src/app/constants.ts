60abaf34567e0f4b875461c61d9f104bacf99e45
Sanjay K <2001ksanjay@gmail.com>
2025-03-24 18:35:20 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'af5527ed88c254c2caf6a52be9a5f46f-997367121.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://af5527ed88c254c2caf6a52be9a5f46f-997367121.ap-south-1.elb.amazonaws.com/api/home',
 };
